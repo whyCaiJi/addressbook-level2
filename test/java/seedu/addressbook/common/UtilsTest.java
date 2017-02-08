af0255ddc8e4b9f0800be8a86d4207463c01a5c3
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-02-08 23:15:09 +0800

    Use JUnit

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -3,6 +3,7 @@ package seedu.addressbook.common;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,28 +14,38 @@ public class UtilsTest {
     public void isAnyNull() {
         // empty list
         assertFalse(Utils.isAnyNull());
+        assertFalse(Utils.isAnyNull(new ArrayList<Object>()));
 
         // Any non-empty list
         assertFalse(Utils.isAnyNull(new Object(), new Object()));
         assertFalse(Utils.isAnyNull("test"));
         assertFalse(Utils.isAnyNull(""));
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(2);
+        assertFalse(Utils.isAnyNull(intList));
 
         // non empty list with just one null at the beginning
         assertTrue(Utils.isAnyNull((Object) null));
         assertTrue(Utils.isAnyNull(null, "", new Object()));
         assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
+        assertTrue(Utils.isAnyNull(null, intList));
 
         // non empty list with nulls in the middle
         assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
         assertTrue(Utils.isAnyNull("", null, new Object()));
+        assertTrue(Utils.isAnyNull(intList, null, new Object()));
 
         // non empty list with one null as the last element
         assertTrue(Utils.isAnyNull("", new Object(), null));
         assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
+        assertTrue(Utils.isAnyNull(intList, new Object(), null));
 
         // confirms nulls inside the list are not considered
+        intList.add(null);
         List<Object> nullList = Arrays.asList((Object) null);
         assertFalse(Utils.isAnyNull(nullList));
+        assertFalse(Utils.isAnyNull(intList));
     }
 
     @Test
@@ -47,12 +58,17 @@ public class UtilsTest {
         assertAreUnique(1);
         assertAreUnique("");
         assertAreUnique("abc");
+        assertAreUnique(new Integer(0));
 
         // all objects unique
         assertAreUnique("abc", "ab", "a");
         assertAreUnique(1, 2);
+        assertAreUnique(new Object(), new Object());
 
         // some identical objects
+        String a = "asdf";
+        String b = "asdf";
+        assertNotUnique(a, b);
         assertNotUnique("abc", "abc");
         assertNotUnique("abc", "", "abc", "ABC");
         assertNotUnique("", "abc", "a", "abc");
